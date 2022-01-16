6a2acc053bad5fd147b3302dffeb50e4637ad5e4
alizamld <moldalieva.aliza@gmail.com>
2022-01-17 04:32:52 +0600

    search

--- main/views.py
+++ main/views.py
@@ -13,63 +13,13 @@ from rest_framework.viewsets import ModelViewSet
 from main.models import *
 from main.permissions import IsAuthor
 from main.serializers import *
-#
-#
-# class PermissionMixin:
-#
-#     def get_permissions(self):
-#         if self.action in ['update', 'partial_update', 'delete', 'destroy']:
-#             permissions= [IsAuthor, ]
-#         elif self.action == 'favorite':
-#             permissions = [IsAuthenticated]
-#         else:
-#             permissions = []
-#         return [permission() for permission in permissions]
-#
-#
-# class PostViewSet(PermissionMixin,ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#
-#     @action(methods=['GET'], detail=False)
-#     def search(self, request):
-#         q = request.query_params.get('q')
-#         queryset = self.get_queryset().filter(
-#             Q(name__icontains=q)|
-#             Q(description__icontains=q)|
-#             Q(country__icontains=q)
-#         )
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data)
-#
-#     @action(detail=False, methods=['get'])
-#     def favorites(self, request):
-#         queryset = Favorite.objects.all()
-#         queryset = queryset.filter(user=request.user)
-#         serializer = FavoriteSerializer(queryset, many=True, context={'request': request})
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     @action(detail=True, methods=['get'], permission_classes=[IsAuthenticated])
-#     def favorite(self, request, pk=None):
-#         hotel = self.get_object()
-#         obj, created = Favorite.objects.get_or_create(user=request.user, hotel=hotel, )
-#         if not created:
-#             obj.favorite = not obj.favorite
-#             obj.save()
-#         favorites = 'added to favorites' if obj.favorite else 'removed from favorites'
-#
-#         return Response(f'Successfully {favorites}', status=status.HTTP_200_OK)
-#
-#     def get_serializer_context(self):
-#         return {'request': self.request, 'action': self.action}
-#
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    filter_backends = [SearchFilter, DjangoFilterBackend]
-    search_fields = ['title', 'description', 'category__name']
-    # filterset_class = PostFilter
+    # filter_backends = [SearchFilter, DjangoFilterBackend]
+    # search_fields = ['title', 'description', 'category__name']
+    # # filterset_class = PostFilter
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -97,6 +47,16 @@ class PostViewSet(ModelViewSet):
         for image in images:
             PostImage.objects.create(post=post, image=image)
         return Response(serializer.data, status=201)
+
+    @action(detail=False, methods=['GET'])
+    def search(self, request):
+        # request_params = request.GET (возвращает словарь, в ктр хранится искомое слово
+        q = request.query_params.get('q')
+        print(q)
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     # чтобы привязать к какому-то пути (api/v1/products/pk/reviews/) (по дефолту по названию метода -> путь)
     # отзывы получаем через детали (определенный продукт, после нажатия на продукт видим отзывы), поэтому True
